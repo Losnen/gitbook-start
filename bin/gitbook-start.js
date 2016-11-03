20b5eb1713747900b8eff25199af3159e26fcc52
Samuel Ramos <alu0100815146@ull.edu.es>
2016-11-03 22:11:54 +0000

    Añadiendo código con octonode

--- bin/gitbook-start.js
+++ bin/gitbook-start.js
@@ -1,17 +1,18 @@
 #! /usr/bin/env node
+
 var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs-extended');
 var path = require('path');
 var fs2 = require('fs');
-var shell = require('shelljs/global');
+var shell = require('shelljs');
 var github = require('octonode');
 const readline = require('readline');
 var exec = require('child_process').exec;
 
-const rl = readline.createInterface({
+/*const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+});*/
 
 
 if (argv.n) {
@@ -23,9 +24,9 @@ if (argv.n) {
     });
 
 } else if (argv.d) {
-  var dependencias = ls('./node_modules/').stdout.split("\n");
-  var expresion = /gitbook-start-*/;
-  
+    var dependencias = ls('./node_modules/').stdout.split("\n");
+    var expresion = /gitbook-start-*/;
+
     for (i = 0; i < dependencias.length; i++) {
 
         try {
@@ -40,8 +41,8 @@ if (argv.n) {
         }
     }
 
-} else if (argv.u){
-    var answer=argv.u;
+} else if (argv.u) {
+    /*  var answer=argv.u;
     var token_
     //rl.question('Indique cual es su nombre de usuario, luego pulse enter y escriba su password(se veráde forma oculta): ', (answer) => {
   // TODO: Log the answer in a database
@@ -82,7 +83,7 @@ function puts(error, stdout, stderr) {
                     console.log("Su repo se ha creado con éxito");
                 }
             });
-        
+
         //console.log(respuesta)
         //var response = stdout.token
         //console.log(stdout.token)
@@ -92,13 +93,59 @@ function puts(error, stdout, stderr) {
         rl.close();
         //console.log(stderr);
       }
-      
+
     }
 var id = Math.floor((Math.random() * 10000) + 1)
 var prueba = exec("curl -u " + answer + " -d '{\"scopes\": [\"repo\", \"user\"], \"note\":\"SYTW-"+id+"\"}' https://api.github.com/authorizations",puts);
 //console.log(prueba)
   rl.close();
-//});
+//});*/
+
+    var token_;
+    var promise = new Promise(function(resolve, reject) {
+        github.auth.config({
+            username: "pruebarepo",
+            password: "aitorjoshuasamuel"
+        }).login({
+            scopes: ['user', 'repo'],
+            note: 'Token repo Gitbook'
+        }, (err, id, token) => {
+            if (err) {
+                reject(Error(err)); // status is not 200 OK, so reject
+            } else {
+                resolve(token);
+            }
+        });
+    });
+
+
+    promise.then(function(data) {
+        fs.writeFile('./token.txt', data, function(err) {});
+
+
+        var client = github.client(data);
+        var ghme = client.me();
+
+        client.get('/user', {}, function(err, status, body, headers) {
+            console.log("Email: " + body.email);
+            console.log("Nombre: " + body.name);
+        });
+
+        ghme.repo({
+            "name": argv.u,
+            "description": "This is your first repo",
+        }, function(err, status, body, headers) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Su repo se ha creado con éxito");
+            }
+        });
+
+    }, function(error) {
+        console.log(error);
+
+    });
 
 
 } else {
