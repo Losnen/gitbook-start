776838cf76c364fbd5eb6de2c93c9012a161370b
Joshua Pérez García <alu0100818819@ull.edu.es>
2016-11-03 16:53:34 +0000

    Probando 1,2,3...

--- bin/gitbook-start.js
+++ bin/gitbook-start.js
@@ -42,33 +42,41 @@ if (argv.n) {
 
 } else if (argv.u){
     var answer=argv.u;
-    var respuesta;
-    function puts(error, stdout, stderr) {
+    rl.question('Indique cual es su nombre de usuario, luego pulse enter y escriba su password(se veráde forma oculta): ', (answer) => {
+  // TODO: Log the answer in a database
+  //console.log('Thank you for your valuable feedback:', answer);
+function puts(error, stdout, stderr) {
       if(stdout){
         rl.close();
-         respuesta= JSON.parse(stdout)
-        fs.writeFile('./prueba.txt', respuesta, function(err) {
+        //var json= JSON.stringify(stdout);
+        //console.log(json)
+        var respuesta = JSON.parse(stdout)
+        //console.log(respuesta['token'])
+        fs.writeFile('./token.txt', stdout, function(err) {
+            if( err ){
+                //console.log( err );
+            }
+            else{
+                //console.log('Se ha escrito correctamente');
+            }
         });
+        //console.log(respuesta)
+        //var response = stdout.token
+        //console.log(stdout.token)
+        //console.log(response);
       }
       if(stderr){
         rl.close();
+        //console.log(stderr);
       }
       
     }
 var id = Math.floor((Math.random() * 10000) + 1)
 var prueba = exec("curl -u " + answer + " -d '{\"scopes\": [\"repo\", \"user\"], \"note\":"+id+"}' https://api.github.com/authorizations",puts);
-
-/*fs2.readFile('./prueba.txt', 'utf8', function (err,token) {
-  if (err) {
-    console.log(err);
-  }
-  var client = github.client(respuesta['token']);
-
-  client.get('/user', {}, function (err, status, body, headers) {
-    console.log(body); //json object
-  });
+//console.log(prueba)
+  rl.close();
 });
-*/
+
 
 } else {
 
