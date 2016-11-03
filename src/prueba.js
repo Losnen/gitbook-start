4e5363d72c7b54ae7d7252cc20a3659787485ccc
Samuel Ramos <alu0100815146@ull.edu.es>
2016-11-03 17:08:18 +0000

    Añadiendo prueba de crear repo

--- src/prueba.js
+++ src/prueba.js
@@ -1,12 +1,26 @@
 var github = require('octonode');
 var fs = require('fs')
-fs.readFile('./.ghtoken', 'utf8', function (err,token) {
-  if (err) {
-    console.log(err);
-  }
-  var client = github.client(token.trim());
+fs.readFile('./.ghtoken', 'utf8', function(err, token) {
+    if (err) {
+        console.log(err);
+    }
 
-  client.get('/user', {}, function (err, status, body, headers) {
-    console.log(body); //json object
-  });
+    var client = github.client(token.trim());
+    var ghme = client.me();
+
+    client.get('/user', {}, function(err, status, body, headers) {
+        console.log("Email: " + body.email);
+        console.log("Nombre: " + body.name);
+    });
+
+    ghme.repo({
+        "name": "Hello-World",
+        "description": "This is your first repo",
+    }, function(err, status, body, headers) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Su repo se ha creado con éxito");
+        }
+    });
 });
